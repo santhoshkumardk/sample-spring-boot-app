3800b9fc34f9319938b48f2223b7b883aa440d3a
Santhosh Kumar DK <santhosh_dk@msn.com>
2024-06-10 22:47:46 -0400

    init

---
+++ src/main/java/com/example/sample_spring_boot_app/SampleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAppApplication.class, args);
+	}
+
+}
